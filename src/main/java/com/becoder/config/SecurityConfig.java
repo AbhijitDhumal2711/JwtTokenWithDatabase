03963beb316f8a28df5edde5178c1ec4b992be53
AbhijitDhumal2711 <abhijitdhumal2711@gmail.com>
2024-09-28 11:55:41 +0530

    First Commit

--- src/main/java/com/becoder/config/SecurityConfig.java
+++ src/main/java/com/becoder/config/SecurityConfig.java
@@ -1,90 +1,35 @@
 package com.becoder.config;
 
- import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.becoder.jwt.JwtAuthenticationEntryPoint;
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
-
-	@Autowired
-	private JwtFilter jwtFilter;
 	
-	 @Autowired
-	    private JwtAuthenticationEntryPoint point;
-
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-//		//		http.csrf(customizer -> customizer.disable());
-//		http.authorizeHttpRequests(
-//		request -> request.requestMatchers("login", "register").permitAll().anyRequest().authenticated());
-//http.formLogin(Customizer.withDefaults());
-//http.httpBasic(Customizer.withDefaults());
-		
-		
-
-//http.csrf(customizer -> customizer.disable())
-//.authorizeHttpRequests(request -> request.requestMatchers("login", "register").permitAll()
-//		.anyRequest().authenticated())
-//.httpBasic(Customizer.withDefaults())
-//.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//return http.build();
 
 
+    @Autowired
+    private JwtAuthenticationEntryPoint point;
+    @Autowired
+    private JwtAuthenticationFilter filter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-http.csrf(csrf -> csrf.disable())
-        .authorizeRequests().
-        requestMatchers("/test").authenticated().requestMatchers("/login").permitAll()
-        .anyRequest()
-        .authenticated()
-        .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-return http.build();
-
-
-	}
-
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-		return daoAuthenticationProvider;
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
-
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests()
+                .requestMatchers("/test").authenticated().requestMatchers("/auth/login").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
